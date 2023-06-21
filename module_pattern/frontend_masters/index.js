329060807aca667524564e0abb7be8a310747af2
ZoranUTF8 <janjiczoran23@gmail.com>
2023-06-21 22:00:22 +0900

    singleton,module examples

---
+++ module_pattern/frontend_masters/index.js
@@ -0,0 +1,9 @@
+// Encapsulation: The values within a module are scoped to that specific module. Values that aren't explicitly exported are not available outside of the module.
+
+// Reusability: We can reuse modules throughout our application
+import { sum, subtract, divide, multiply } from "./functions.js";
+
+console.log("Sum", sum(1, 2));
+console.log("Subtract", subtract(1, 2));
+console.log("Divide", divide(1, 2));
+console.log("Multiply", multiply(1, 2));
